e89fc2c8401f5f31d7351020a6c907ee29854138
David Le <davidkle@gmail.com>
2016-10-29 11:36:44 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './source/App.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      },
+      include: [
+        path.join(__dirname, '/source')
+      ]
+    }],
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
+  }
+};
