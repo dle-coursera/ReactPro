073ccf9b09a37799ac548cf2ba72ac65c002778f
David Le <davidkle@gmail.com>
2016-10-29 23:28:56 -0700

    Added chapter 2

--- source/Card.jsx
+++ source/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import marked from 'marked';
 import CheckList from './CheckList';
 
 class Card extends React.Component {
@@ -9,12 +10,26 @@ class Card extends React.Component {
     };
   }
 
+  toggleDetails() {
+    this.setState({showDetails: !this.state.showDetails});
+  }
+
   render() {
+    let sideColor = {
+      position: `absolute`,
+      zIndex: -1,
+      top: 0,
+      bottom: 0,
+      left: 0,
+      width: 7,
+      backgroundColor: this.props.color
+    }
+
     let cardDetails;
     if (this.state.showDetails) {
       cardDetails = (
         <div className="card__details">
-          {this.props.description}
+          <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}}/>
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
       );
@@ -22,9 +37,10 @@ class Card extends React.Component {
 
     return (
       <div className="card">
-        <div className="card__title" onClick={ ()=>
-          this.setState({showDetails:!this.state.showDetails})
-        }>
+        <div style={sideColor}/>
+        <div className={
+          this.state.showDetails? "card__title card__title--is-open": "card__title"
+          } onClick={this.toggleDetails.bind(this)}>
           {this.props.title}
         </div>
         {cardDetails}
@@ -34,3 +50,5 @@ class Card extends React.Component {
 }
 
 module.exports = Card;
+
+
